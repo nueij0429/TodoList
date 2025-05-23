a9d64343309975a5644f5e4e7fee46328d5a2235
nueij0429 <ssjjjieun0429@gmail.com>
2025-05-23 18:39:28 +0900

    step2 완료

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import '@components/TodoItem.css';
 
 import { connect } from 'react-redux';
-import { removeTodo, toggleTodo } from '@/actions';
+//import { removeTodo, toggleTodo } from '@/actions';
+import { removeTodo, toggleTodo } from '@/reducers/todoSlice';
 
 class TodoItem extends Component {
     /*
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,13 @@ import App from '@/App.jsx'
 //import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
-import { toDoReducer } from './reducers';
+//import { toDoReducer } from './reducers';
+import todoSlice from '@/reducers/todoSlice';
 
 //const store = createStore(toDoReducer, applyMiddleware(thunk));
 const store = configureStore({
-  reducer: toDoReducer,
+  //reducer: toDoReducer,
+  reducer: todoSlice
   //middleware: getDefaultMiddleware => getDefaultMiddleware().concat(thunk)
 });
 
